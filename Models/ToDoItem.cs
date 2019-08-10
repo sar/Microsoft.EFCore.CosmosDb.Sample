2391d487a97991e6b756c5d896245d7791d96ab3
Sarthak Malik <malik@sarthak.ca>
2019-08-10 12:20:00 -0400

    Property name attributes referencing System.Text.Json.Serialization

--- Models/ToDoItem.cs
+++ Models/ToDoItem.cs
@@ -1,24 +1,23 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Microsoft.EFCore.CosmosDb.Sample.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.Json.Serialization;
     using Newtonsoft.Json;
 
     public class ToDoItem
     {
         [Key]
-        [JsonProperty(PropertyName = "id")]
+        [JsonPropertyName("id")]
         public Guid? Id { get; set; }
 
-        [JsonProperty(PropertyName = "name")]
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
-        [JsonProperty(PropertyName = "description")]
+        [JsonPropertyName("description")]
         public string Description { get; set; }
 
-        [JsonProperty(PropertyName = "isComplete")]
+        [JsonPropertyName("complete")]
         public bool Completed { get; set; }
     }
 }
