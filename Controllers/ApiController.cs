e4fc0a5092e7a3c7fd44b9f838d71d7fdddff0f1
Sarthak Malik <malik@sarthak.ca>
2019-08-10 12:00:00 -0400

    Created PostSeedAsync method for List<ToDoItem> sample insertion

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -67,6 +67,59 @@ namespace Microsoft.EFCore.CosmosDb.Sample.Controllers
             }
         }
 
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status201Created)]
+        public async Task<string> PostSeedAsync()
+        {
+            List<ToDoItem> sampleToDoList = new List<ToDoItem>
+            {
+                new ToDoItem
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Virtual Golfing with Remote Team",
+                    Description =
+                        "Engage the remote team in a virtual golfing tournament at the local multi-player arena.",
+                    Completed = false,
+                },
+                new ToDoItem
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Book International Flight to Sea-Tac",
+                    Description = "Departure from Toronto (CYYZ) to destination Seattle (KSEA) sometime next month.",
+                    Completed = false,
+                },
+                new ToDoItem
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Tour Seattle Space Needle",
+                    Description =
+                        "The Seattle Space Needle is a landmark observation tower and a must visit location for any tourist.",
+                    Completed = false,
+                },
+            };
+
+            try
+            {
+                foreach (ToDoItem item in sampleToDoList)
+                {
+                    await using (ToDoItemsContext context = new ToDoItemsContext(_options, _configuration))
+                    {
+                        await context.ToDoItems.AddAsync(item);
+                        await context.SaveChangesAsync();
+                    }
+                }
+
+                return $"Iterated {nameof(ToDoItem)} in list {nameof(sampleToDoList)} for {nameof(EntityEntry)} type.";
+            }
+            catch (CosmosException ce)
+            {
+                Console.WriteLine(ce);
+            }
+
+            return null;
+        }
+
         [HttpGet]
         [Route("[action]")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
